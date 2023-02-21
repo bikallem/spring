04f0d257d659893632630f1601ce8e4051c3298f
Bikal Lem <gbikal@gmail.com>
2023-02-21 15:04:27 +0000

    lib_spring : add method module

---
+++ lib_spring/method.ml
@@ -0,0 +1,15 @@
+type 'a t = { name : string }
+
+let make (type a) name (_x : a) : a t = { name }
+let get = { name = "get" }
+let head = { name = "head" }
+let delete = { name = "delete" }
+let options = { name = "options" }
+let trace = { name = "trace" }
+let post = { name = "post" }
+let put = { name = "put" }
+let patch = { name = "patch" }
+let connect = { name = "connect" }
+let equal a b = String.equal a.name b.name
+let to_string t = t.name
+let pp fmt t = Format.fprintf fmt "%s" (String.uppercase_ascii t.name)
---
+++ lib_spring/method.mli
@@ -0,0 +1,15 @@
+type 'a t
+
+val make : string -> 'a -> 'a t
+val get : Body.none t
+val head : Body.none t
+val delete : Body.none t
+val options : Body.none t
+val trace : Body.none t
+val post : 'a t
+val put : 'a t
+val patch : 'a t
+val connect : Body.none t
+val equal : 'a t -> 'b t -> bool
+val to_string : 'a t -> string
+val pp : Format.formatter -> 'a t -> unit
