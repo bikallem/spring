88e218bd0bde1d9801f5738c5529638db0ba29fb
Bikal Lem <gbikal@gmail.com>
2023-02-23 19:51:52 +0000

    spring : add Multipart to Spring namespace

--- lib/spring.ml
+++ lib/spring.ml
@@ -6,10 +6,15 @@ module Transfer_encoding_hdr = Transfer_encoding_hdr
 module Date = Date
 module Header = Header
 include Header.H
+
+(* Body *)
 module Body = Body
+module Chunked_body = Chunked_body
+module Multipart = Multipart
+
+(* Others *)
 module Status = Status
 include Status.S
-module Chunked_body = Chunked_body
 module Request = Request
 module Response = Response
 module Client = Client
--- lib/spring.mli
+++ lib/spring.mli
@@ -6,10 +6,17 @@ module Transfer_encoding_hdr = Transfer_encoding_hdr
 module Date = Date
 module Header = Header
 include module type of Header.H
+
+(** {1 Body} *)
+
 module Body = Body
+module Chunked_body = Chunked_body
+module Multipart = Multipart
+
+(* {1 Others} *)
+
 module Status = Status
 include module type of Status.S
-module Chunked_body = Chunked_body
 module Request = Request
 module Response = Response
 module Client = Client
