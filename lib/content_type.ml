28980b11c56c596fec447e7a423ceabc9243999e
Bikal Lem <gbikal@gmail.com>
2023-02-25 21:47:30 +0000

    content_type : use Astring

--- lib/content_type.ml
+++ lib/content_type.ml
@@ -1,9 +1,9 @@
-module M = Map.Make (String)
+open Astring
 
 type t =
   { type_ : string
   ; sub_type : string
-  ; parameters : string M.t
+  ; parameters : string String.Map.t
   }
 
 type media_type = string * string
@@ -12,14 +12,14 @@ open Buf_read.Syntax
 open Buf_read
 
 let p r =
-  let type_ = token r in
-  let sub_type = (char '/' *> token) r in
+  let type_ = token r |> String.Ascii.lowercase in
+  let sub_type = (char '/' *> token) r |> String.Ascii.lowercase in
   let parameters = parameters r in
-  let parameters = M.of_seq @@ List.to_seq parameters in
+  let parameters = String.Map.of_seq @@ List.to_seq parameters in
   { type_; sub_type; parameters }
 
 let make ?(params = []) (type_, sub_type) =
-  let parameters = M.of_seq @@ List.to_seq params in
+  let parameters = String.Map.of_seq @@ List.to_seq params in
   { type_; sub_type; parameters }
 
 let decode v = p (of_string v)
@@ -29,7 +29,7 @@ let encode t =
   Buffer.add_string buf t.type_;
   Buffer.add_string buf "/";
   Buffer.add_string buf t.sub_type;
-  M.iter
+  String.Map.iter
     (fun name value ->
       Buffer.add_string buf "; ";
       Buffer.add_string buf name;
@@ -40,6 +40,7 @@ let encode t =
 
 let media_type t = (t.type_, t.sub_type)
 
-let find_param t name = M.find_opt (String.lowercase_ascii name) t.parameters
+let find_param t name =
+  String.Map.find_opt (String.Ascii.lowercase name) t.parameters
 
-let charset t = M.find_opt "charset" t.parameters
+let charset t = String.Map.find_opt "charset" t.parameters
