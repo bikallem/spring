75e0e473c640d23c3bd6b2ee08a7abb2a77c35f7
Bikal Lem <gbikal@gmail.com>
2023-02-27 22:35:18 +0000

    cookie : decode() correctly parses many cookies

--- lib/cookie.ml
+++ lib/cookie.ml
@@ -8,15 +8,28 @@ let cookie_pair s =
   let value = cookie_value s in
   (name, value)
 
+let rec skip_ws s =
+  if s.pos < String.length s.i then
+    match String.get s.i s.pos with
+    | ' ' | '\t' ->
+      accept s 1;
+      skip_ws s
+    | _ -> ()
+  else ()
+
 let decode v =
   let s = { i = v; pos = 0 } in
-  let rec aux () =
+  let rec aux cookies =
     if s.pos < String.length s.i then
       match String.get s.i s.pos with
       | ';' ->
         accept s 1;
-        cookie_pair s :: aux ()
-      | _ -> []
-    else []
+        skip_ws s;
+        let cookie = cookie_pair s in
+        aux (cookie :: cookies)
+      | _ -> cookies
+    else cookies
   in
-  aux ()
+  let cookie = cookie_pair s in
+  aux [ cookie ]
+
--- lib/cookie.mli
+++ lib/cookie.mli
@@ -2,6 +2,6 @@
     https://datatracker.ietf.org/doc/html/rfc6265#section-4.2 *)
 
 (** [t] represents a HTTP cookie. *)
-type t
+type t = (string * string) list
 
 val decode : string -> t
