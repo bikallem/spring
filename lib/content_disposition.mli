6dc45f4d312e7b698b648b977a540985ee7d8865
Bikal Lem <gbikal@gmail.com>
2023-03-01 10:47:36 +0000

    content_disposition : api doc t

--- lib/content_disposition.mli
+++ lib/content_disposition.mli
@@ -1,6 +1,7 @@
 (** [Content_disposition] implements [Content-Disposition] header as specified
     in https://httpwg.org/specs/rfc6266.html#top *)
 
+(** [t] is the [Content-Disposition] header value. *)
 type t
 
 val make : ?params:(string * string) list -> string -> t
