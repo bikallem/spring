7059ecfeca5945a827aa7a1713a424a40578be91
Bikal Lem <gbikal@gmail.com>
2023-03-01 13:40:34 +0000

    method : api doc for t/make/get

--- lib/method.mli
+++ lib/method.mli
@@ -1,9 +1,19 @@
+(** [Method] implements HTTP request method as specified in
+    https://httpwg.org/specs/rfc9110.html#methods *)
+
 type lowercase_string = private string
 
+(** [t] represents an instance of HTTP request method. Its textual
+    representation is an ASCII lowercase string. *)
 type t = lowercase_string
 
+(** [make meth] creates a HTTP request method [t] represented by [meth]. *)
 val make : string -> t
 
+(** {1 Methods} *)
+
+(** [get] is HTTP GET method as defined in
+    https://httpwg.org/specs/rfc9110.html#GET *)
 val get : t
 
 val head : t
