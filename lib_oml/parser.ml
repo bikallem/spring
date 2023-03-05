604c51733463b8b65d72b72c55532286425648b0
Bikal Lem <gbikal@gmail.com>
2023-03-05 17:24:38 +0000

    oml/parser : parse attributes

--- lib_oml/parser.ml
+++ lib_oml/parser.ml
@@ -1,26 +1,30 @@
 let nul = '\000'
 
 type tok =
-  | End_elem_start (* </ *)
-  | Comment_elem (* <!-- *)
   | Start_elem (* < *)
-  | Elem_slash_close (* /> *)
-  | Elem_close (* > *)
+  | Elem_close (* > - Both start end end element *)
+  | Start_elem_slash_close (* /> *)
+  | End_elem_start (* </ *)
+  | Comment_elem_start (* <!-- *)
+  | Comment_elem_end (* --> *)
   | Code_block_start (* { *)
   | Code_block_end (* } *)
-  | Data (* element tag name *)
+  | Data of char (* element tag name *)
+  | SPACE (* whitespace *)
   | Equal (* = *)
   | Eoi (* End of input *)
 
 let tok_to_string = function
-  | End_elem_start -> "END_ELEM_START"
-  | Comment_elem -> "COMMENT_ELEM"
   | Start_elem -> "START_ELEM"
-  | Elem_slash_close -> "ELEM_SLASH_CLOSE"
+  | Start_elem_slash_close -> "START_ELEM_SLASH_CLOSE"
+  | End_elem_start -> "END_ELEM_START"
+  | Comment_elem_start -> "COMMENT_ELEM_START"
+  | Comment_elem_end -> "COMMENT_ELEM_END"
   | Elem_close -> "ELEM_CLOSE"
   | Code_block_start -> "CODE_BLOCK_START"
   | Code_block_end -> "CODE_BLOCK_END"
-  | Data -> "DATA"
+  | Data c -> "DATA " ^ Char.escaped c
+  | SPACE -> "SPACE"
   | Equal -> "EQUAL"
   | Eoi -> "EOF"
 
@@ -33,27 +37,9 @@ type input =
   ; i : unit -> char (* input function *)
   }
 
-let next i =
-  try
-    let c = i.i () in
-    match c with
-    | '\n' ->
-      i.col <- 1;
-      i.line <- i.line + 1;
-      i.c <- c
-    | _ ->
-      i.col <- i.col + 1;
-      i.c <- c
-  with End_of_file -> i.c <- nul
-
 let is_ascii_whitespace = function
   | '\x09' | '\x0A' | '\x0C' | '\x0D' | '\x20' -> true
   | _ -> false
-
-let rec skip_ws i =
-  if is_ascii_whitespace i.c then (
-    next i;
-    skip_ws i)
 
 let err lbl msg (i : input) =
   failwith
@@ -77,52 +63,93 @@ let is_digit = function
 let is_alpha_num = function
   | c -> is_alpha c || is_digit c
 
-let add_c i = Buffer.add_char i.buf i.c
+let add_c c i = Buffer.add_char i.buf c
 
 let clear i = Buffer.clear i.buf
 
+let _pf = Printf.printf
+
+let next i =
+  try
+    let c = i.i () in
+    match c with
+    | '\n' ->
+      i.col <- 1;
+      i.line <- i.line + 1;
+      i.c <- c
+    | _ ->
+      i.col <- i.col + 1;
+      i.c <- c
+  with End_of_file -> i.c <- nul
+
 let tok i =
-  skip_ws i;
   match i.c with
   | '<' -> (
     next i;
     match i.c with
     | '/' ->
-      next i;
-      i.tok <- End_elem_start
+      i.tok <- End_elem_start;
+      next i
     | '!' ->
-      expect_c '-' i;
       next i;
       expect_c '-' i;
       next i;
-      i.tok <- Comment_elem
+      expect_c '-' i;
+      i.tok <- Comment_elem_start;
+      next i
     | _ -> i.tok <- Start_elem)
   | '/' ->
     next i;
     expect_c '>' i;
-    next i;
-    i.tok <- Elem_slash_close
+    i.tok <- Start_elem_slash_close;
+    next i
   | '>' ->
+    i.tok <- Elem_close;
+    next i
+  | '{' ->
+    i.tok <- Code_block_start;
+    next i
+  | '}' ->
+    i.tok <- Code_block_end;
+    next i
+  | '=' ->
+    i.tok <- Equal;
+    next i
+  | c when is_ascii_whitespace c ->
+    let rec aux () =
+      match i.c with
+      | c when is_ascii_whitespace c ->
+        next i;
+        aux ()
+      | _ -> ()
+    in
     next i;
-    i.tok <- Elem_close
-  | '{' -> i.tok <- Code_block_start
-  | '}' -> i.tok <- Code_block_end
-  | '=' -> i.tok <- Equal
+    aux ();
+    i.tok <- SPACE
   | c when c = nul -> i.tok <- Eoi
-  | c when is_alpha c || c = '_' -> i.tok <- Data
-  | c -> err "tok" ("unrecognized character '" ^ Char.escaped c ^ "'") i
+  | c ->
+    i.tok <- Data c;
+    next i
+
+let rec skip_ws i =
+  match i.tok with
+  | SPACE ->
+    tok i;
+    skip_ws i
+  | _ -> ()
+
+(* use this to move from one token to another. *)
 
 let string_input s =
   let len = String.length s in
   let pos = ref (-1) in
   let i () =
     incr pos;
-    if !pos = len then raise End_of_file else String.get s !pos
+    if !pos >= len then raise End_of_file else String.get s !pos
   in
   let input =
-    { buf = Buffer.create 10; line = 1; col = 0; c = nul; tok = Eoi; i }
+    { buf = Buffer.create 10; line = 1; col = 0; c = i (); tok = Eoi; i }
   in
-  next input;
   tok input;
   input
 
@@ -133,32 +160,19 @@ let expect_tok tok i =
       ("expecting " ^ tok_to_string tok ^ " but got " ^ tok_to_string i.tok)
       i
 
-let _pf = Printf.printf
-
 (* Attribute parsing *)
 
 let rec attribute_name i =
   match i.c with
-  | ' ' | '=' | '>' ->
-    let name = Buffer.contents i.buf in
+  | '\x00' .. '\x1F' | '\x7F' .. '\x9F' | '"' | '\'' | '>' | '/' | '=' | ' ' ->
+    let nm = Buffer.contents i.buf in
     clear i;
-    name
-  | '/' -> (
-    next i;
-    match i.c with
-    | '>' ->
-      let v = Buffer.contents i.buf in
-      clear i;
-      i.tok <- Elem_slash_close;
-      v
-    | _ ->
-      err "attribute_name" ("expected '>', got '" ^ Char.escaped i.c ^ "'") i)
-  | ('\x00' .. '\x1F' | '\x7F' .. '\x9F' | '"' | '\'') as c ->
-    err "attribute_name"
-      ("invalid attribute name character '" ^ Char.escaped c ^ "'")
-      i
+    (*     _pf "\nname:%s, '%c'%!" nm i.c; *)
+    tok i;
+    nm
   | _ ->
-    add_c i;
+    (*     _pf "\nname:%c%!" i.c; *)
+    add_c i.c i;
     next i;
     attribute_name i
 
@@ -167,153 +181,123 @@ let rec quoted_attribute_value c i =
     let v = Buffer.contents i.buf in
     clear i;
     next i;
+    tok i;
     v)
   else (
-    add_c i;
+    add_c i.c i;
     next i;
     quoted_attribute_value c i)
 
 let rec unquoted_attribute_value i =
   match i.c with
-  | ' ' ->
+  | c when is_ascii_whitespace c || c = '>' || c = '/' ->
     let v = Buffer.contents i.buf in
     clear i;
+    tok i;
     v
-  | '>' ->
-    let v = Buffer.contents i.buf in
-    clear i;
-    i.tok <- Elem_close;
-    v
-  | '/' -> (
-    next i;
-    match i.c with
-    | '>' ->
-      let v = Buffer.contents i.buf in
-      clear i;
-      i.tok <- Elem_slash_close;
-      v
-    | _ ->
-      err "unquoted_attribute_value"
-        ("expected '>', got '" ^ Char.escaped i.c ^ "'")
-        i)
   | '"' | '\'' | '=' | '<' | '`' ->
     err "unquoted_attribute_value"
       ("invalid attribute value character '" ^ Char.escaped i.c ^ "'")
       i
-  | c when is_ascii_whitespace c ->
-    err "unquoted_attribute_value"
-      ("invalid attribute value character '" ^ Char.escaped i.c ^ "'")
-      i
   | _ ->
-    add_c i;
+    add_c i.c i;
     next i;
     unquoted_attribute_value i
 
-let rec code_attribute_value i =
+let rec code_attribute i =
   match i.c with
   | '}' ->
-    let code_block = Buffer.contents i.buf in
+    let v = Buffer.contents i.buf in
     clear i;
     next i;
-    code_block
+    tok i;
+    v
   | _ ->
-    add_c i;
+    add_c i.c i;
     next i;
-    code_attribute_value i
+    code_attribute i
 
-let attribute_value name i =
-  let validate_value v =
-    if String.(equal empty v) then
-      err "attribute_value" "empty attribute value not allowed" i
-  in
-  match i.c with
-  | ('\'' | '"') as c ->
-    next i;
-    let value = quoted_attribute_value c i in
-    validate_value value;
-    Node.attribute name value
-  | '{' ->
-    next i;
-    let code = code_attribute_value i in
-    validate_value code;
-    Node.attribute_code_value name code
-  | _ ->
-    let value = unquoted_attribute_value i in
-    validate_value value;
-    Node.attribute name value
+let attribute_value i =
+(*   _pf "\nattr value: %s%!" (tok_to_string i.tok); *)
+  match i.tok with
+  | Equal -> (
+    tok i;
+    skip_ws i;
+    match i.tok with
+    | Data c when c = '\'' ->
+      let v = quoted_attribute_value c i in
+      Some (Node.single_quoted_attribute_value v)
+    | Data c when c = '"' ->
+      let v = quoted_attribute_value c i in
+      Some (Node.double_quoted_attribute_value v)
+    | Data c ->
+      add_c c i;
+      let v = unquoted_attribute_value i in
+      Some (Node.unquoted_attribute_value v)
+    | Code_block_start ->
+      let v = code_attribute i in
+      Some (Node.code_attribute_value v)
+    | _ ->
+      err "attribute_value"
+        ("expected ' \" '{', got'" ^ tok_to_string i.tok ^ "'")
+        i)
+  | _ -> None
 
 let attributes i =
   let attributes = Queue.create () in
   let rec aux () =
+    skip_ws i;
     match i.tok with
-    | Data -> (
-      let name = attribute_name i in
-      tok i;
-      match i.tok with
-      | Equal ->
-        next i;
-        skip_ws i;
-        let v = attribute_value name i in
-        Queue.add v attributes;
-        tok i;
-        aux ()
-      | Elem_close | Elem_slash_close ->
-        Queue.add (Node.bool_attr name) attributes
-      | Data | Code_block_start ->
-        Queue.add (Node.bool_attr name) attributes;
-        aux ()
-      | _ ->
-        err "attributes"
-          ("expected token '=', '>' or '/>', got '" ^ tok_to_string i.tok ^ "'")
-          i)
     | Code_block_start ->
-      next i;
-      let rec code_attr () =
-        match i.c with
-        | '}' ->
-          let code_block = Buffer.contents i.buf in
-          clear i;
-          next i;
-          if String.(equal empty code_block) then ()
-          else Queue.add (Node.code_attribute code_block) attributes
-        | _ ->
-          add_c i;
-          next i;
-          code_attr ()
-      in
-      code_attr ();
-      tok i;
+      let v = code_attribute i in
+      Queue.add (Node.code_attribute v) attributes;
       aux ()
+    | Data c -> (
+      add_c c i;
+      let name = attribute_name i in
+(*       _pf "\nattribute name: %s%!" name; *)
+      skip_ws i;
+      match attribute_value i with
+      | Some v ->
+        Queue.add (Node.attribute name v) attributes;
+        aux ()
+      | None ->
+        Queue.add (Node.bool_attr name) attributes;
+        aux ())
     | _ -> ()
   in
   aux ();
-  Queue.to_seq attributes |> List.of_seq
+  if Queue.length attributes > 0 then Queue.to_seq attributes |> List.of_seq
+  else []
 
 (* Element/Code Element/Comment/Text parsing *)
 let tag i =
   let rec aux () =
-    match i.c with
-    | c when is_alpha_num c ->
-      add_c i;
-      next i;
-      aux ()
-    | '_' | '\'' | '.' ->
-      add_c i;
-      next i;
+    match i.tok with
+    | Data c when is_alpha_num c || c = '_' || c = '\'' || c = '.' ->
+      add_c c i;
+      tok i;
       aux ()
     | _ ->
       let tag = Buffer.contents i.buf in
       clear i;
       tag
   in
-  expect_tok Data i;
-  add_c i;
-  next i;
-  aux ()
+  skip_ws i;
+  match i.tok with
+  | Data c when is_alpha c || c = '_' ->
+    add_c c i;
+    tok i;
+    aux ()
+  | Data c ->
+    err "tag"
+      ("tag name starts with alpha or '_' character, got '" ^ Char.escaped c
+     ^ "'")
+      i
+  | _ -> err "tag" ("expected DATA token, got '" ^ tok_to_string i.tok ^ "'") i
 
 let end_elem start_tag i =
   let tag = tag i in
-  tok i;
   expect_tok Elem_close i (* > *);
   if String.equal start_tag tag then ()
   else
@@ -321,116 +305,145 @@ let end_elem start_tag i =
       ("expected closed tag '" ^ start_tag ^ "', got '" ^ tag ^ "'")
       i
 
-let is_void_elem = function
-  | "area"
-  | "base"
-  | "br"
-  | "col"
-  | "embed"
-  | "hr"
-  | "img"
-  | "input"
-  | "link"
-  | "meta"
-  | "param"
-  | "source"
-  | "track"
-  | "wbr" -> true
-  | _ -> false
-
-let void_elem_close i =
+(* <div > or <div /> *)
+let start_tag i =
+  skip_ws i;
+  expect_tok Start_elem i;
+  tok i;
+  let tag_name = tag i in
+  skip_ws i;
+  let attributes = attributes i in
+  skip_ws i;
   match i.tok with
-  | Elem_close | Elem_slash_close -> ()
+  | Start_elem_slash_close ->
+    tok i;
+    (tag_name, attributes, false)
+  | Elem_close ->
+    tok i;
+    (tag_name, attributes, true)
   | tok ->
-    err "void_elem_close"
-      ("expecting '" ^ tok_to_string Elem_close ^ "' or '"
-      ^ tok_to_string Elem_slash_close
-      ^ "', got '" ^ tok_to_string tok ^ "'.")
+    err "element"
+      ("expecting '"
+      ^ tok_to_string Start_elem_slash_close
+      ^ "' or '" ^ tok_to_string Elem_close ^ "', got '" ^ tok_to_string tok
+      ^ "'")
+      i
+
+(* </div> *)
+let end_tag tag_name i =
+  skip_ws i;
+  expect_tok End_elem_start i;
+  tok i;
+  let tag = tag i in
+  expect_tok Elem_close i (* > *);
+  tok i;
+  if String.equal tag_name tag then ()
+  else
+    err "close_tag"
+      ("expected closed tag '" ^ tag_name ^ "', got '" ^ tag ^ "'")
       i
 
 let rec element i =
-  expect_tok Start_elem i;
-  tok i;
-  expect_tok Data i;
-  let tag_name = tag i in
-  tok i;
-  let attributes = attributes i in
-  if is_void_elem tag_name then (
-    void_elem_close i;
-    Node.void ~attributes tag_name)
-  else
-    match i.tok with
-    | Elem_slash_close -> Node.element ~attributes tag_name (* no children *)
-    | Elem_close -> (
-      tok i;
-      match i.tok with
-      | End_elem_start (* </ *) ->
-        tok i;
-        end_elem tag_name i;
-        Node.element ~attributes tag_name
-      | Start_elem | Code_block_start ->
-        let children = children tag_name i in
-        Node.element ~attributes ~children tag_name
-      | tok ->
-        err "element"
-          ("expecting '"
-          ^ tok_to_string End_elem_start
-          ^ "' or '" ^ tok_to_string Start_elem ^ "', got '" ^ tok_to_string tok
-          ^ "'")
-          i)
-    | tok ->
-      err "element"
-        ("expecting '"
-        ^ tok_to_string Elem_slash_close
-        ^ "' or '" ^ tok_to_string Elem_close ^ "', got '" ^ tok_to_string tok
-        ^ "'")
-        i
+  let tag_name, attributes, has_children = start_tag i in
+  if has_children then (
+(*     _pf "\nelement: start children %s%!" tag_name; *)
+    let children = children i in
+    let e = Node.element ~attributes ~children tag_name in
+(*     _pf "\nelement: end children: %s:%d%!" tag_name (List.length children); *)
+    end_tag tag_name i;
+    e)
+  else Node.element ~attributes ~children:[] tag_name
 
-and children start_tag i =
-  let rec aux acc =
+and children i =
+  let children = Queue.create () in
+  let rec aux () =
+(*     _pf "\nchildren: %s%!" (tok_to_string i.tok); *)
     match i.tok with
-    | Start_elem -> aux (element i :: acc)
-    | End_elem_start ->
-      tok i;
-      end_elem start_tag i;
-      acc
-    | Code_block_start -> aux (code_element i :: acc)
-    | _ -> acc
+    | Start_elem ->
+      let el = element i in
+      Queue.add el children;
+      aux ()
+    | Code_block_start ->
+      let el = code_element i in
+      Queue.add el children;
+      aux ()
+    | Comment_elem_start ->
+      () (*
+      Queue.add (comment_element i) children;
+      aux ()
+*)
+    | _ -> ()
   in
-  aux []
+  aux ();
+  Queue.to_seq children |> List.of_seq
 
-(* OCaml code blocks starts with '{' and ends with '}' *)
+(* { ... } *)
 and code_element i =
-  let rec aux parsing_code_block q =
+  let code_blocks = Queue.create () in
+  let rec aux parsing_code_block =
+    (*     _pf "\ncode_element: %c%!" i.c; *)
     match i.c with
     | '}' ->
-      prepend_code_block parsing_code_block i q;
+      prepend_code_block parsing_code_block i;
       next i;
-      i.tok <- Code_block_end;
-      q
+      tok i
     | '<' ->
-      prepend_code_block parsing_code_block i q;
+      prepend_code_block parsing_code_block i;
       next i;
       i.tok <- Start_elem;
-      Queue.add (element i) q;
-      aux false q
+      Queue.add (element i) code_blocks;
+      aux false
     | _ ->
-      add_c i;
+      add_c i.c i;
       next i;
-      aux true q
-  and prepend_code_block is_parsing_code_block i q =
+      aux true
+  and prepend_code_block is_parsing_code_block i =
     if is_parsing_code_block then (
       let code = Buffer.contents i.buf in
       clear i;
-      Queue.add (Node.code_block code) q)
+      Queue.add (Node.code_block code) code_blocks)
   in
   expect_tok Code_block_start i;
+  add_c i.c i;
   next i;
-  let code_blocks =
-    aux false (Queue.create ()) |> Queue.to_seq |> List.of_seq
-  in
-  expect_tok Code_block_end i;
-  tok i;
+  aux false;
+  let code_blocks = Queue.to_seq code_blocks |> List.of_seq in
   Node.code_element code_blocks
 
-let root i = element i
+and comment_element i =
+  (*   _pf "%c%!" i.c; *)
+  match i.c with
+  | '-' -> (
+    next i;
+    (*     _pf "%c%!" i.c; *)
+    match i.c with
+    | '-' -> (
+      next i;
+      (*       _pf "%c%!" i.c; *)
+      match i.c with
+      | '>' ->
+        (*         _pf "end\n%!"; *)
+        let txt = Buffer.contents i.buf in
+        clear i;
+        next i;
+        tok i;
+        Node.comment txt
+      | _ ->
+        Buffer.add_char i.buf '-';
+        Buffer.add_char i.buf '-';
+        (*         add_c i; *)
+        next i;
+        comment_element i)
+    | _ ->
+      Buffer.add_char i.buf '-';
+      (*       add_c i; *)
+      next i;
+      comment_element i)
+  | _ ->
+    (*     add_c i; *)
+    next i;
+    comment_element i
+
+let root i =
+  let e = element i in
+  e
