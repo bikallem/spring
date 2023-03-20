887e48ccf828e4b88bd9c36e5f6a6a25425ad08b
Bikal Lem <gbikal@gmail.com>
2023-03-20 14:58:48 +0000

    spring/bin : accept OHTML_FILE arg

--- bin/spring.ml
+++ bin/spring.ml
@@ -8,8 +8,12 @@ let ohtml_cmd =
     ]
   in
   let info = Cmd.info "ohtml" ~version:"%%VERSION%%" ~doc ~man in
-  let ohtml () = print_string "Ohtml generate" in
-  let ohtml_t = Term.(const ohtml $ const ()) in
+  let ohtml_file_arg =
+    let doc = ".ohtml filename" in
+    Arg.(required & pos 0 (some' string) None & info [] ~docv:"OHTML_FILE" ~doc)
+  in
+  let ohtml filepath = Printf.printf "Generating Spring view %s" filepath in
+  let ohtml_t = Term.(const ohtml $ ohtml_file_arg) in
   Cmd.v info ohtml_t
 
 let spring_cmd =
@@ -22,7 +26,7 @@ let spring_cmd =
   let info = Cmd.info "spring" ~version:"%%VERSION%%" ~doc ~man in
   let spring () = print_string "spring says hello" in
   let spring_t = Term.(const spring $ const ()) in
-  Cmd.group ~default:spring_t info [ohtml_cmd]
+  Cmd.group ~default:spring_t info [ ohtml_cmd ]
 
 let main () = exit (Cmd.eval spring_cmd)
 let () = main ()
