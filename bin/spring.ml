69b2fea1ec75720c35b6b848be719204e2320d0f
Bikal Lem <gbikal@gmail.com>
2023-03-20 13:47:34 +0000

    spring/bin : start cli functionality

--- bin/spring.ml
+++ bin/spring.ml
@@ -1,3 +1,28 @@
-(* open Cmdliner *)
+open Cmdliner
 
-let () = print_string "hello world"
+let ohtml_cmd =
+  let doc = "Generate Spring view from .ohtml template file" in
+  let man =
+    [ `S Manpage.s_bugs
+    ; `P "Email bug reports to github.com/bikallem/spring/issues."
+    ]
+  in
+  let info = Cmd.info "ohtml" ~version:"%%VERSION%%" ~doc ~man in
+  let ohtml () = print_string "Ohtml generate" in
+  let ohtml_t = Term.(const ohtml $ const ()) in
+  Cmd.v info ohtml_t
+
+let spring_cmd =
+  let doc = "Spring" in
+  let man =
+    [ `S Manpage.s_bugs
+    ; `P "Email bug reports to github.com/bikallem/spring/issues."
+    ]
+  in
+  let info = Cmd.info "spring" ~version:"%%VERSION%%" ~doc ~man in
+  let spring () = print_string "spring says hello" in
+  let spring_t = Term.(const spring $ const ()) in
+  Cmd.group ~default:spring_t info [ohtml_cmd]
+
+let main () = exit (Cmd.eval spring_cmd)
+let () = main ()
