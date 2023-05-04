a033d01bf0abe11f4ad9f725eb3196a55831e6b4
Bikal Lem <gbikal@gmail.com>
2023-05-04 18:23:47 +0100

    lib/examples/hello : use Server.get to configure router

--- examples/hello/hello.ml
+++ examples/hello/hello.ml
@@ -1,24 +1,17 @@
 open Spring
 
-let hello _req =
+let say_hello _req =
   let v = V.layout_v ~title:"Hello Page" ~body:V.hello_v in
   Response.ohtml v
 
-let router : Spring.Server.pipeline =
- fun next req ->
-  match Spring.Request.resource req with
-  | "/" -> hello req
-  | "/products" ->
-    let body = V.products_v [ "apple"; "oranges"; "bananas" ] in
-    let v = V.layout_v ~title:"Products Page" ~body in
-    Response.ohtml v
-  | _ -> next req
+let display_products _req =
+  let body = V.products_v [ "apple"; "oranges"; "bananas" ] in
+  let v = V.layout_v ~title:"Products Page" ~body in
+  Response.ohtml v
 
 let () =
   Eio_main.run @@ fun env ->
-  let handler : Spring.Server.handler =
-    Spring.Server.strict_http env#clock
-    @@ router @@ Spring.Server.not_found_handler
-  in
-  let server = Spring.Server.make ~on_error:raise env#clock env#net handler in
-  Spring.Server.run_local ~port:8080 server
+  Server.routed_server ~on_error:raise env#clock env#net
+  |> Server.get [%r "/"] say_hello
+  |> Server.get [%r "/products"] display_products
+  |> Server.run_local ~port:8080
