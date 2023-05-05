414390159d131f0c655fa470a99f111b2cd271f7
Bikal Lem <gbikal@gmail.com>
2023-05-05 18:19:56 +0100

    lib/cookie : document API

--- lib/cookie.mli
+++ lib/cookie.mli
@@ -2,9 +2,12 @@
     https://datatracker.ietf.org/doc/html/rfc6265#section-4.2 *)
 
 type t
-(** [t] represents a HTTP cookie. *)
+(** [t] represents a HTTP cookie. A cookie can hold 1 or more values indexed via
+    a case-sensitive cookie name. *)
 
 val decode : string -> t
+(** [decode s] decodes [s] into [t]. *)
+
 val encode : t -> string
 
 (** {1 Find Cookies} *)
