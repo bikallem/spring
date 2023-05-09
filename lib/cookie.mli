0cb3babc46f1772f2e787a6fde9c248f88afecb5
Bikal Lem <315907+bikallem@users.noreply.github.com>
2023-05-09 22:57:44 +0100

    lib/cookie : update typo in doc

--- lib/cookie.mli
+++ lib/cookie.mli
@@ -2,7 +2,7 @@
     https://datatracker.ietf.org/doc/html/rfc6265#section-4.2 *)
 
 type t
-(** [t] represents a HTTP cookie. A cookie can hold 1 or more values indexed via
+(** [t] represents a HTTP cookie. A cookie can hold one or more values indexed via
     a case-sensitive cookie name. *)
 
 val decode : string -> t
