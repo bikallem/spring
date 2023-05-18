c71c5dcf836297ee8f5cda3e70ad3ae9486d2f55
Bikal Lem <gbikal@gmail.com>
2023-05-18 18:05:01 +0100

    chore : add flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Euler Dev environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell =
+          pkgs.mkShell { packages = [ pkgs.gmp pkgs.cargo pkgs.rustc ]; };
+      });
+}
