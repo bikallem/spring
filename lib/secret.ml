542f18eeb4a4380243b36d0ec0bf8890c92771f7
Bikal Lem <gbikal@gmail.com>
2023-05-19 17:01:56 +0100

    lib/secret : encrypt/decrypt functions

---
+++ lib/secret.ml
@@ -0,0 +1,27 @@
+let nonce_size = 12
+
+let encrypt_base64 key contents =
+  assert (String.length contents > 0);
+  let key = Mirage_crypto.Chacha20.of_secret key in
+  let nonce = Mirage_crypto_rng.generate nonce_size in
+  let encrypted =
+    Mirage_crypto.Chacha20.authenticate_encrypt ~key ~nonce
+      (Cstruct.of_string contents)
+  in
+  Cstruct.concat [ nonce; encrypted ]
+  |> Cstruct.to_string
+  |> Base64.(encode_string ~pad:false ~alphabet:uri_safe_alphabet)
+
+let decrypt_base64 key contents =
+  assert (String.length contents > 0);
+  let key = Mirage_crypto.Chacha20.of_secret key in
+  let contents =
+    Base64.(decode_exn ~pad:false ~alphabet:uri_safe_alphabet contents)
+    |> Cstruct.of_string
+  in
+  let nonce = Cstruct.sub contents 0 nonce_size in
+  Cstruct.sub contents nonce_size (Cstruct.length contents - nonce_size)
+  |> Mirage_crypto.Chacha20.authenticate_decrypt ~key ~nonce
+  |> function
+  | Some s -> Cstruct.to_string s
+  | None -> failwith "Unable to decrypt contents"
