0e3f6ff54349644da171339e825b482d2088f869
Bikal Lem <gbikal@gmail.com>
2023-05-23 11:27:09 +0100

    examples/hello : update to use cookie_session

--- examples/hello/hello.ml
+++ examples/hello/hello.ml
@@ -7,8 +7,10 @@ let display_products _req =
   |> V.view ~title:"Products Page"
 
 let () =
+  Printexc.record_backtrace true;
   Eio_main.run @@ fun env ->
-  Server.app_server ~on_error:raise env#clock env#net
+  Server.app_server ~on_error:raise ~secure_random:env#secure_random env#clock
+    env#net
   |> Server.get [%r "/"] say_hello
   |> Server.get [%r "/products"] display_products
   |> Server.run_local ~port:8080
