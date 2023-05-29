0f5444fdbe8d28860344bd5e4164bafe0ae7cf77
Bikal Lem <gbikal@gmail.com>
2023-05-29 12:30:02 +0100

    lib/context : move context type to Context.t

---
+++ lib/context.mli
@@ -0,0 +1,18 @@
+type t
+(** [t] represents request handler data context. It encapsulates data request
+    and session data. *)
+
+val make : ?session_data:Session.session_data -> Request.server_request -> t
+
+val session_data : t -> Session.session_data option
+(** [session_data ctx] is [Some v] if session_data is populated by one of the
+    request pipelines. Otherwise is it is [None].
+
+    See {!val:session_pipeline}. *)
+
+val replace_session_data : Session.session_data -> t -> t
+(** [replace_context_session_data session_data t] is [t] with session data in
+    [t] replaced by [session_data]. *)
+
+val request : t -> Request.server_request
+(** [request t] is the HTTP request instance. *)
