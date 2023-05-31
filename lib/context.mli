13f2a88c42c4ae44a9b6972a52b645cf403a6942
Bikal Lem <gbikal@gmail.com>
2023-05-31 13:56:59 +0100

    lib/context : add 'Session' section in api doc

--- lib/context.mli
+++ lib/context.mli
@@ -8,19 +8,21 @@ val make : ?session_data:Session.session_data -> Request.server_request -> t
     @param session_data
       is the session data of the context. Default value is [None] *)
 
+val request : t -> Request.server_request
+(** [request t] is the HTTP request instance. *)
+
+(** {1 Session} *)
+
 val session_data : t -> Session.session_data option
 (** [session_data ctx] is [Some v] if session_data is populated by one of the
     request pipelines. Otherwise is it is [None].
 
     See {!val:session_pipeline}. *)
 
-val replace_session_data : Session.session_data -> t -> unit
-(** [replace_context_session_data session_data t] is [t] with session data in
-    [t] replaced by [session_data]. *)
-
-val request : t -> Request.server_request
-(** [request t] is the HTTP request instance. *)
-
 val new_session : t -> t
 (** [new_session t] is [t = make (request t)] where [t] is initialized with an
     empty session data. *)
+
+val replace_session_data : Session.session_data -> t -> unit
+(** [replace_context_session_data session_data t] is [t] with session data in
+    [t] replaced by [session_data]. *)
