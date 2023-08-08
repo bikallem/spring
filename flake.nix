4ff18de5a3c9959a8884dd31a79064d6eec84602
Bikal Lem <gbikal@gmail.com>
2023-08-08 10:46:51 +0545

    flake : update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
 {
-  description = "Euler Dev environment";
+  description = "Spring Dev environment";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell =
-          pkgs.mkShell { packages = [ pkgs.gmp pkgs.cargo pkgs.rustc ]; };
-      });
+      in { devShell = pkgs.mkShell { packages = [ pkgs.gmp ]; }; });
 }
