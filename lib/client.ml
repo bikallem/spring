3d1d791b64403649bdf624c600fea25a05789480
Bikal Lem <gbikal@gmail.com>
2023-08-08 17:40:28 +0545

    lib/client : https/tls support

--- lib/client.ml
+++ lib/client.ml
@@ -5,7 +5,7 @@
 
 (* Connection cache using Hashtbl. *)
 module Cache = Hashtbl.Make (struct
-  type t = host * service
+  type t = Uri.scheme * host * service
 
   and host = string (* eg. www.example.com *)
 
@@ -16,7 +16,7 @@ module Cache = Hashtbl.Make (struct
   let hash = Hashtbl.hash
 end)
 
-type conn = < Eio.Net.stream_socket ; Eio.Flow.close >
+type conn = Eio.Flow.two_way
 
 type connection_stream =
   host_connection_count
@@ -67,17 +67,39 @@ let tcp_connect sw ~host ~service net =
          | `Unix _ -> None)
     |> aux
   with
-  | conn -> conn
+  | conn -> (conn :> Eio.Flow.two_way)
   | exception (Eio.Exn.Io _ as ex) ->
     let bt = Printexc.get_raw_backtrace () in
     Eio.Exn.reraise_with_context ex bt "connecting to %S:%s" host service
 
-let connection t ((host, service) as k) =
+let tls_authenticator =
+  let authenticator_ref = ref None in
+  fun () ->
+    match !authenticator_ref with
+    | Some x -> x
+    | None -> (
+      match Ca_certs.authenticator () with
+      | Ok a ->
+        authenticator_ref := Some a;
+        a
+      | Error (`Msg m) -> invalid_arg ("failed to trust anchors: " ^ m))
+
+let tls_connect flow =
+  let authenticator = tls_authenticator () in
+  let config = Tls.Config.client ~authenticator () in
+  (Tls_eio.client_of_flow config flow :> Eio.Flow.two_way)
+
+let connection t ((scheme, host, service) as k) =
   Eio.Mutex.lock t.mutex;
   Fun.protect ~finally:(fun () -> Eio.Mutex.unlock t.mutex) @@ fun () ->
   match Cache.find_opt t.cache k with
   | Some (n, s) when n <= t.maximum_conns_per_host && Eio.Stream.length s = 0 ->
     let conn = tcp_connect t.sw ~host ~service t.net in
+    let conn =
+      match scheme with
+      | `Http -> conn
+      | `Https -> tls_connect conn
+    in
     Cache.replace t.cache k (n + 1, s);
     conn
   | Some (_, s) -> Eio.Stream.take s
@@ -105,58 +127,50 @@ let do_call t (req : request) f =
     | Some x -> string_of_int x
     | None -> "80"
   in
-  let k = (host, service) in
+  let scheme = Request.scheme req in
+  let k = (scheme, host, service) in
   Eio.Time.Timeout.run_exn t.timeout @@ fun () ->
   let conn = connection t k in
-  Eio.Buf_write.with_flow ~initial_size:t.write_initial_size conn
-  @@ fun writer ->
-  Request.write_client_request req writer;
-  let initial_size = t.read_initial_size in
-  let buf_read = Buf_read.of_flow ~initial_size ~max_size:max_int conn in
-  let res = Response.parse_client_response buf_read in
-  let x = f res in
-  Eio.Mutex.lock t.mutex;
-  Fun.protect
-    (fun () ->
-      Response.close res;
-      let _n, s = Cache.find t.cache k in
-      Eio.Stream.add s conn;
-      x)
-    ~finally:(fun () -> Eio.Mutex.unlock t.mutex)
+  Eio.Buf_write.with_flow ~initial_size:t.write_initial_size conn (fun writer ->
+      Request.write_client_request req writer;
+      let initial_size = t.read_initial_size in
+      let buf_read = Buf_read.of_flow ~initial_size ~max_size:max_int conn in
+      let res = Response.parse_client_response buf_read in
+      let x = f res in
+      Eio.Mutex.lock t.mutex;
+      Fun.protect
+        (fun () ->
+          Response.close res;
+          let _n, s = Cache.find t.cache k in
+          Eio.Stream.add s conn;
+          x)
+        ~finally:(fun () -> Eio.Mutex.unlock t.mutex))
 
 type uri = string
 
 let parse_uri uri =
-  if String.is_prefix ~affix:"https" uri then
-    raise @@ Invalid_argument "uri: https protocol not supported";
   let uri =
-    if String.is_prefix ~affix:"http" uri || String.is_prefix ~affix:"http" uri
-    then uri
-    else "http://" ^ uri
+    if String.is_prefix ~affix:"http" uri then uri else "http://" ^ uri
   in
   match Uri.absolute_uri uri with
   | uri ->
+    let scheme = Uri.absolute_uri_scheme uri in
     let host, port = Uri.host_and_port uri in
     let host = Host.make ?port host in
     let path, query = Uri.absolute_uri_path_and_query uri in
     let resource = Uri.pct_encode ?query path in
-    (host, resource)
+    (scheme, host, resource)
   | exception _ -> invalid_arg "[uri] invalid HTTP uri."
 
-let get t uri =
-  let host, resource = parse_uri uri in
-  let req = Request.make_client_request ~resource host Method.get Body.none in
-  do_call t req
+let make_request ?(body = Body.none) meth (scheme, host, resource) =
+  Request.make_client_request ~scheme ~resource host meth body
 
-let head t uri =
-  let host, resource = parse_uri uri in
-  let req = Request.make_client_request ~resource host Method.head Body.none in
-  do_call t req
+let get t uri = parse_uri uri |> make_request Method.get |> do_call t
+
+let head t uri = parse_uri uri |> make_request Method.head |> do_call t
 
 let post t body uri =
-  let host, resource = parse_uri uri in
-  let req = Request.make_client_request ~resource host Method.post body in
-  do_call t req
+  parse_uri uri |> make_request ~body Method.post |> do_call t
 
 let post_form_values t form_values uri =
   let body = Body.writable_form_values form_values in
