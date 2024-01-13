dad95cd1fe7bda9ec00a98ec9b081a50c5e58527
Bikal Lem <gbikal@gmail.com>
2024-01-13 22:18:31 +0000

    flake.nix : nix flakes dev environment

--- flake.nix
+++ flake.nix
@@ -1,92 +1,111 @@
 {
   description = "OCaml lib and bin projects to get started with nix flakes.";
 
-  # inputs.nix-filter.url = "github:numtide/nix-filter";
-  # inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs = {
-      url = "github:nix-ocaml/nix-overlays";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    nix-filter.url = "github:numtide/nix-filter";
-  };
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages_5_1.overrideScope'
-            (oself: osuper: {
-              eio = osuper.eio.overrideAttrs (_: _: rec {
-                version = "0.11";
-                src = super.fetchurl {
+        pkgs = nixpkgs.legacyPackages."${system}".extend (final: prev: {
+          ocamlPackages = prev.ocaml-ng.ocamlPackages_5_1.overrideScope'
+            (ofinal: oprev: rec {
+              eio = with oprev;
+                buildDunePackage rec {
+                  pname = "eio";
+                  version = "0.11";
+                  minimalOCamlVersion = "5.0";
+
+                  src = prev.fetchurl {
+                    url =
+                      "https://github.com/ocaml-multicore/${pname}/releases/download/v${version}/${pname}-${version}.tbz";
+                    hash =
+                      "sha256-DDN0IHRWJjFneIb0/koC+Wcs7JQpf/hcLthU21uqcao=";
+                  };
+
+                  propagatedBuildInputs = [
+                    bigstringaf
+                    cstruct
+                    domain-local-await
+                    fmt
+                    hmap
+                    lwt-dllist
+                    mtime
+                    optint
+                    psq
+                  ];
+
+                  checkInputs = [ alcotest crowbar mdx ];
+
+                  nativeCheckInputs = [ mdx.bin ];
+
+                  meta = {
+                    homepage =
+                      "https://github.com/ocaml-multicore/ocaml-${pname}";
+                    changelog =
+                      "https://github.com/ocaml-multicore/ocaml-${pname}/raw/v${version}/CHANGES.md";
+                    description = "Effects-Based Parallel IO for OCaml";
+                    license = with prev.lib.licenses; [ isc ];
+                    maintainers = with prev.lib.maintainers; [ toastal ];
+                  };
+                };
+
+              eio_linux = oprev.eio_linux.override { eio = eio; };
+              eio_posix = oprev.eio_posix.override { eio = eio; };
+              eio_main = oprev.eio_main.override { eio = eio; };
+
+              mirage-crypto = oprev.mirage-crypto.overrideAttrs (_: _: rec {
+                version = "0.11.1";
+                src = prev.fetchurl {
                   url =
-                    "https://github.com/ocaml-multicore/eio/releases/download/v${version}/eio-${version}.tbz";
-                  hash = "sha256-DDN0IHRWJjFneIb0/koC+Wcs7JQpf/hcLthU21uqcao=";
+                    "https://github.com/mirage/mirage-crypto/releases/download/v${version}/mirage-crypto-${version}.tbz";
+                  sha256 =
+                    "sha256-DNoUeyCpK/cMXJ639VxnXQOrx2u9Sx8N2c9/w4AW0pw=";
                 };
               });
 
-              # eio_linux = opkgs.eio_linux.override { eio = eio; };
-              # eio_posix = opkgs.eio_posix.override { eio = eio; };
-              # eio_main = opkgs.eio_main.override {
-              #   eio = eio;
-              #   eio_posix = eio_posix;
-              #   eio_linux = eio_linux;
-              # };
+              mirage-crypto-rng = oprev.mirage-crypto-rng.override {
+                mirage-crypto = mirage-crypto;
+              };
 
-              # mirage-crypto = opkgs.mirage-crypto.overrideAttrs (_: _: rec {
-              #   version = "0.11.1";
-              #   src = pkgs.fetchurl {
-              #     url =
-              #       "https://github.com/mirage/mirage-crypto/releases/download/v${version}/mirage-crypto-${version}.tbz";
-              #     sha256 =
-              #       "sha256-DNoUeyCpK/cMXJ639VxnXQOrx2u9Sx8N2c9/w4AW0pw=";
-              #   };
-              # });
+              mirage-crypto-rng-eio = oprev.buildDunePackage {
+                pname = "mirage-crypto-rng-eio";
+                inherit (mirage-crypto) src version;
+                dontDetectOcamlConflicts = true;
+                propagatedBuildInputs = [ eio mirage-crypto-rng oprev.mtime ];
+              };
 
-              # mirage-crypto-rng = opkgs.mirage-crypto-rng.override {
-              #   mirage-crypto = mirage-crypto;
-              #   # mtime = mtime;
-              # };
+              tls = oprev.tls.overrideAttrs (_: _: rec {
+                version = "0.17.1";
+                src = prev.fetchurl {
+                  url =
+                    "https://github.com/mirleft/ocaml-tls/releases/download/v${version}/tls-${version}.tbz";
+                  hash = "sha256-gBDStt4UjaIoaSgYHSM71yD6YPoVez1CULyg3QCMXT8=";
+                };
+              });
 
-              # mirage-crypto-rng-eio = opkgs.buildDunePackage {
-              #   pname = "mirage-crypto-rng-eio";
-              #   inherit (mirage-crypto) src version;
-              #   propagatedBuildInputs = [ eio mirage-crypto-rng opkgs.mtime ];
-              # };
-
-              # tls = opkgs.tls.overrideAttrs (_: _: rec {
-              #   version = "0.17.1";
-              #   src = pkgs.fetchurl {
-              #     url =
-              #       "https://github.com/mirleft/ocaml-tls/releases/download/v${version}/tls-${version}.tbz";
-              #     hash = "sha256-gBDStt4UjaIoaSgYHSM71yD6YPoVez1CULyg3QCMXT8=";
-              #   };
-              # });
-
-              # tls-eio = opkgs.buildDunePackage {
-              #   pname = "tls-eio";
-              #   inherit (tls) src meta version;
-              #   propagatedBuildInputs = [
-              #     tls
-              #     mirage-crypto-rng
-              #     mirage-crypto-rng-eio
-              #     opkgs.x509
-              #     eio
-              #   ];
-              # };
+              tls-eio = oprev.buildDunePackage {
+                pname = "tls-eio";
+                inherit (tls) src meta version;
+                dontDetectOcamlConflicts = true;
+                propagatedBuildInputs = [
+                  tls
+                  mirage-crypto-rng
+                  mirage-crypto-rng-eio
+                  oprev.x509
+                  eio
+                ];
+              };
 
             });
         });
-
         opkgs = pkgs.ocamlPackages;
-
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
-          # dontDetectOcamlConflicts = true;
+          dontDetectOcamlConflicts = true;
           nativeBuildInputs = with opkgs; [
             dune_3
             utop
@@ -98,20 +117,19 @@
           ];
 
           packages = with opkgs; [
-            logs
-            # eio
-            # eio_main
+            eio
+            eio_main
             ptime
             menhir
             menhirLib
             tls
-            # tls-eio
+            tls-eio
             ipaddr
             ca-certs
             x509
             ppxlib
-            # mirage-crypto-rng
-            # mirage-crypto-rng-eio
+            mirage-crypto-rng
+            mirage-crypto-rng-eio
             astring
             base64
             cmdliner
